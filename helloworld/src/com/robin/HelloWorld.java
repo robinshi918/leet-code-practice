e456187e89a9663acd2bb9762d69aad8c24d9978
史云 <shiyun@doumi.com>
2016-04-19 20:19:55 +0800

    add Eclipse project files

--- helloworld/src/com/robin/HelloWorld.java
+++ helloworld/src/com/robin/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		Solution solution = new Solution();
 		
 		ListNode head = new ListNode(1);
-		head.append(1).append(1);//.append(3).append(1).append(2).append(3).append(4).append(4);
+		head.append(1).append(1).append(3).append(1).append(2).append(3).append(4).append(4);
 		System.out.println(head.toString());
 		
 		solution.deleteDuplicates(head);
